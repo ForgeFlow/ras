6731f77dd7c508d7201d036f403b11b5b525982d
lurobe94 <luis.romero@eficent.com>
2018-05-24 08:31:26 +0000

    update working

--- launcher.py
+++ launcher.py
@@ -3,10 +3,10 @@ import os
 import deletesubstring
 
 if __name__ == '__main__':
-    update =  menu_test2.foo()
+    update =  menu_test2.m_functionality()
     print "UPDATE: " + str(update)
     if update == True:
-        os.system('sh /home/pi/Raspberry/update_repo.sh')
+        os.system('sh /home/pi/Raspberry_Code/update_repo.sh')
         deletesubstring.del_update()
         os.system('sudo reboot')
 
--- menu_test2.py
+++ menu_test2.py
@@ -34,7 +34,19 @@ pos = 0
 enter = False
 reset = False
 
-dic = {" ": [" ",0,1,0,0], 'check_in': ['CHECKED IN',14,1,0,0], 'check_out': ['CHECKED OUT',6,1,0,0], 'FALSE': ['NOT AUTHORIZED',47,2,14,0], 'Bye!': ['BYE!',45,1,0,0], 'Wifi1': ['WiFi Setting',45,2,30,0], 'Wifi2': ['Connect to 10.0.0.1:9191',25,3,55,8], 'Wifi3': ['using RaspiWifi setup',35,3,20,37], 'update': ['Resetting to update',20,3,60,35], 'config1': ['Connect to 192.168.1.47',25,3,55,8], 'config2': ['for the configuration',53,3,53,5]}
+def get_ip():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    try:
+        # doesn't even have to be reachable
+        s.connect(('10.255.255.255', 1))
+        IP = s.getsockname()[0]
+    except:
+        IP = '127.0.0.1'
+    finally:
+        s.close()
+    return IP
+
+dic = {' ': [" ",0,1,0,0,24], 'check_in': ['CHECKED IN',14,1,0,0,24], 'check_out': ['CHECKED OUT',6,1,0,0,24], 'FALSE': ['NOT AUTHORIZED',47,2,14,0,24], 'Bye!': ['BYE!',45,1,0,0,24], 'Wifi1': ['WiFi Setting',45,2,30,0,24], 'Wifi2': ['Connect to 10.0.0.1:9191',25,3,55,8,24], 'Wifi3': ['using RaspiWifi setup',35,3,20,37,24], 'update': ['Resetting to update',20,3,55,35,24], 'config1': ['Connect to ' + get_ip(),25,3,55,15,20], 'config2': ['for device configuration',53,3,35,7,20]}
 
 
 # Create an object of the class MFRC522
@@ -70,6 +82,7 @@ def scan_card(MIFAREReader,odoo):
     global port
     global msg
     global adm, turn_off
+    global admin_id
 
     # Scan for cards
     (status,TagType) = MIFAREReader.MFRC522_Request(MIFAREReader.PICC_REQIDL)
@@ -89,7 +102,7 @@ def scan_card(MIFAREReader,odoo):
         card = hex(int(uid[0])).split('x')[-1] + hex(int(uid[1])).split('x')[-1] + hex(int(uid[2])).split('x')[-1] + hex(int(uid[3])).split('x')[-1] 
 
         print card
-        if card == "1a25ad79":
+        if card == admin_id:
             adm = True
             turn_off = True
         # This is the default key for authentication
@@ -188,7 +201,7 @@ def screen_drawing(device,info):
     # use custom font
     font_path = os.path.abspath(os.path.join(os.path.dirname(__file__),
                                 'fonts', 'C&C Red Alert [INET].ttf'))
-    font2 = ImageFont.truetype(font_path, 24)
+    font2 = ImageFont.truetype(font_path, dic[info][5])
 
    # print "DIC: " + dic[info][0] + str(dic[info][1])
 
@@ -196,16 +209,16 @@ def screen_drawing(device,info):
         draw.rectangle(device.bounding_box, outline="white")
         try:
             if dic[info][2] == 1:
-                draw.text((dic[info][1], 20), dic[info][0], font=font2, fill="white")
+                draw.text((dic[info][1], 20+(24-dic[info][5])/2), dic[info][0], font=font2, fill="white")
             elif dic[info][2] == 2:
                 a, b = dic[info][0].split(" ")
-                draw.text((dic[info][1], 7), a, font=font2, fill="white")
-                draw.text((dic[info][3], 33), b, font=font2, fill="white")
+                draw.text((dic[info][1], 7+(24-dic[info][5])/2), a, font=font2, fill="white")
+                draw.text((dic[info][3], 33+(24-dic[info][5])/2), b, font=font2, fill="white")
             else:
                 a, b, c = dic[info][0].split(" ")
-                draw.text((dic[info][1], 2), a, font=font2, fill="white")
-                draw.text((dic[info][3], 20), b, font=font2, fill="white")
-                draw.text((dic[info][4], 37), c, font=font2, fill="white")
+                draw.text((dic[info][1], 2+(24-dic[info][5])/2), a, font=font2, fill="white")
+                draw.text((dic[info][3], 20+(24-dic[info][5])/2), b, font=font2, fill="white")
+                draw.text((dic[info][4], 37+(24-dic[info][5])/2), c, font=font2, fill="white")
         except:
             draw.text((20, 20), info, font=font2, fill="white")
 
@@ -281,11 +294,11 @@ def main():
                 elapsed_time = time.time() - start_time
                 menu(device,"Main program","RFID reader","Reset settings","Halt",pos)
        #         try:
-                if elapsed_time > 15.0 and elapsed_time <= 23.0:
+                if elapsed_time > 10.0 and elapsed_time <= 15.0:
                     pos = 1  #key_pressed(pos)
-                elif elapsed_time > 23.0 and elapsed_time <= 30.0:
+                elif elapsed_time > 15.0 and elapsed_time <= 20.0:
                     pos = 0
-                elif elapsed_time > 30.0:
+                elif elapsed_time > 20.0:
                     enter = True
                 else:
                     pos = 0
@@ -323,15 +336,16 @@ def main():
         time.sleep(2)
 
 #if __name__ == "__main__":
-def foo():
+def m_functionality():
     global device
     global update
     global reset
     global host, port, user_name, user_password, dbname
+    global admin_id
     try:
         device = get_device()
         img_path = os.path.abspath(os.path.join(os.path.dirname(__file__),
-            'images', 'ef4.png'))
+            'images', 'ef5.png'))
         logo = Image.open(img_path).convert("RGBA")
         fff = Image.new(logo.mode, logo.size, (0,) * 4)
 
@@ -361,6 +375,7 @@ def foo():
             user_name = json_data["user_name"][0]
             user_password = json_data["user_password"][0]
             dbname = json_data["db"][0]
+            admin_id = json_data["admin_id"][0]
             if "update" not in json_data:
                 update = False
             else:
@@ -375,13 +390,14 @@ def foo():
         if update == True:
             screen_drawing(device,"update")
             time.sleep(2)
-            screen_drawing(device,"")
+            screen_drawing(device," ")
             GPIO.cleanup()
         screen_drawing(device,"Bye!")
         time.sleep(3)
+        screen_drawing(device," ")
         GPIO.cleanup()
         if reset == True:
-            screen_drawing(device,"")
+            screen_drawing(device," ")
             reset_lib.reset_to_host_mode()
         return update
 
