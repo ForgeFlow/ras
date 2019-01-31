4277d81842f3debe6035d561493d3c07149ec566
lubusax <lu.bu.sax@gmail.com>
2019-01-31 19:48:19 +0000

    corrected : data.json empty error; Message Error while communicating with odoo; status odoo and WiFi always updated after swiping card; No Jump anymore while swipping admin card the first time

--- lib/OdooXMLrpc.py
+++ lib/OdooXMLrpc.py
@@ -23,11 +23,28 @@ class OdooXMLrpc():
 
     def set_params(self):
         _logger.debug('Params config is %s ' % os.path.isfile(self.datajson))
-        if os.path.isfile(self.datajson):
+        try:
             j_file = open(self.datajson)
             self.j_data = json.load(j_file)
             j_file.close()
-
+        except Exception as e:
+            _logger.exception(e)
+            if os.path.isfile(self.datajson):
+                os.system('sudo rm ' + self.datajson)
+            # be sure that there is no data.json file
+            # if the data.json can not be loaded
+            self.j_data = False
+            self.db = False
+            self.user = False
+            self.pswd = False
+            self.host = False
+            self.port = False
+            self.adm = False
+            self.tz = False
+            self.https_on = False
+            self.url_template = False
+            self.uid = False
+        else:
             self.db = self.j_data["db"][0]
             self.user = self.j_data["user_name"][0]
             self.pswd = self.j_data["user_password"][0]
@@ -58,19 +75,6 @@ class OdooXMLrpc():
                                      (self.host, self.port))
 
             self.uid = self._get_user_id()
-
-        else:
-            self.j_data = False
-            self.db = False
-            self.user = False
-            self.pswd = False
-            self.host = False
-            self.port = False
-            self.adm = False
-            self.tz = False
-            self.https_on = False
-            self.url_template = False
-            self.uid = False
 
     def _get_object_facade(self, url):
         try:
