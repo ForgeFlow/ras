f3024f440ffdaa1d8117e462c5fc27c57fb4bfa4
hveficent <hector.villarreal@eficent.com>
2019-04-02 12:47:11 +0200

    fixup travis

--- lib/fixup.py
+++ lib/fixup.py
@@ -2,7 +2,7 @@ import os
 
 
 def ensure_autostart_after_crash():
-    end_of_line = '\n'
+    eol = '\n'
 
     fixup1 = 'Restart=always'
     fixup2 = 'RestartSec=3'
@@ -15,14 +15,12 @@ def ensure_autostart_after_crash():
     with open(file_to_be_updated, 'r') as f:
         lines = f.readlines()
         print(lines)
-        if fixup1 + end_of_line not in lines:
+        if fixup1 + eol not in lines:
             f.close()
             with open(file_to_be_updated, 'w') as f:
                 for line in lines:
-                    if line.strip(end_of_line) == index:
-                        f.write(index + end_of_line +
-                                fixup1 + end_of_line +
-                                fixup2 + end_of_line)
+                    if line.strip(eol) == index:
+                        f.write(index + eol + fixup1 + eol + fixup2 + eol)
                     else:
                         f.write(line)
             f.close()
