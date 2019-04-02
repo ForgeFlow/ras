a72fc902c98e2a2438415d4cd7947b73d4b7a3c8
lubusax <lu.bu.sax@gmail.com>
2019-04-02 18:18:55 +0000

    ensure autostart of ras-launcher service after crash

---
+++ lib/fixup.py
@@ -0,0 +1,32 @@
+import os
+
+def ensure_autostart_after_crash():
+    eol = '\n'
+
+    fixup1 = 'Restart=always'
+    fixup2 = 'RestartSec=3'
+
+    index = '[Service]' # insert fixup
+                        # just after index
+
+    file_to_be_updated = '/lib/systemd/system/ras-launcher.service'
+
+    with open(file_to_be_updated, 'r') as f:
+        lines   = f.readlines()
+        print(lines)
+        if fixup1 + eol not in lines:
+            f.close()
+            with open(file_to_be_updated, 'w') as f:
+                for line in lines:
+                    if line.strip(eol) == index:
+                        f.write(index  + eol + \
+                                fixup1 + eol + \
+                                fixup2 + eol )
+                    else:
+                        f.write(line)
+            f.close()
+            os.system('systemctl daemon-reload')
+            os.system('systemctl restart ras-launcher')
+
+#ensure_autostart_after_crash()
+
