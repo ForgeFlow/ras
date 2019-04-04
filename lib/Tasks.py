add7a2c7d4a384ddd25f7b9476d5e93eecee79e5
hveficent <hector.villarreal@eficent.com>
2019-04-04 12:53:02 +0200

    Remove old wifi credentials on wifi restart

--- lib/Tasks.py
+++ lib/Tasks.py
@@ -127,12 +127,12 @@ class Tasks:
             self.back_to_begin_option()
 
     def reset_wifi(self):
-        _logger.debug("Reset WI-FI")
-        self.Disp.display_msg("configure_wifi")
-        os.system("sudo rm -R /etc/NetworkManager/system-connections/*")
-        os.system("sudo wifi-connect --portal-ssid " + SSID_reset)
-        os.system("sudo systemctl restart ras-portal.service")
-        self.Buzz.Play("back_to_menu")
+        _logger.debug('Reset WI-FI')
+        self.Disp.display_msg('configure_wifi')
+        os.system('sudo rm -R /etc/NetworkManager/system-connections/*')
+        os.system('sudo wifi-connect --portal-ssid ' + SSID_reset)
+        os.system('sudo systemctl restart ras-portal.service')
+        self.Buzz.Play('back_to_menu')
         self.back_to_begin_option()
 
     def odoo_config(self):
