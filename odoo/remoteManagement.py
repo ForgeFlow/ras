9e63f4aa7633c52bf737855badc60ed387c0047e
lubusax <lu.bu.sax@gmail.com>
2021-03-30 01:24:10 +0200

    auto

--- odoo/remoteManagement.py
+++ odoo/remoteManagement.py
@@ -48,24 +48,21 @@ def acknowledgeTerminalInOdoo():
     terminal_ID_in_Odoo     = None
     params = Params(db=PARAMS)
     try:
-        if not os.path.isfile(co.PARAMS_DB_TRANSFERRED_FLAG):
-            template = ut.settings["odooUrlTemplate"]
-        else:
-            template = params.get("odooUrlTemplate", encoding='utf-8')
-        requestURL  = template + co.ROUTE_ACK_GATE
+        requestURL  = ut.settings["odooUrlTemplate"] + co.ROUTE_ACK_GATE
         headers     = {'Content-Type': 'application/json'}
-        settings_to_send = [
-            "manufacturingData",
-            "hashed_machine_id",            
-            "firmwareVersion",
-            "language",
-            "ownIpAddress",
-            "ssh",
-            "sshPassword",
-            "showEmployeeName",
-            "timezone",                
-        ]
-        payload     = getPayload(settings_to_send)
+        list_of_all_keys = params.get_list_of_all_keys()
+        # settings_to_send = [
+            #     "manufacturingData",
+            #     "hashed_machine_id",            
+            #     "firmwareVersion",
+            #     "language",
+            #     "ownIpAddress",
+            #     "ssh",
+            #     "sshPassword",
+            #     "showEmployeeName",
+            #     "timezone",                
+            # ]
+        payload     = getPayload(list_of_all_keys)
 
         response    = requests.post(url=requestURL, json=payload, headers=headers)
 
@@ -78,19 +75,22 @@ def acknowledgeTerminalInOdoo():
             if error:
                 loggerINFO(f"could not acknowledge the terminal in Odoo- error: {error}")
             else:
-                terminal_ID_in_Odoo     = answer['id']
-                loggerINFO(f"terminal ID in Odoo: {answer['id']}")
-                ut.storeOptionInDeviceCustomization("terminalIDinOdoo",answer['id'])
-                ut.storeOptionInDeviceCustomization("RASxxx",getRASxxx())
-                ut.storeOptionInDeviceCustomization("routefromDeviceToOdoo",answer["routefromDeviceToOdoo"])
-                ut.storeOptionInDeviceCustomization("routefromOdooToDevice",answer["routefromOdooToDevice"])
-                ut.storeOptionInDeviceCustomization("shouldGetFirmwareUpdate",answer["shouldGetFirmwareUpdate"])
-                ut.storeOptionInDeviceCustomization("location",answer["location"])
+                keys_to_pop_out = ["terminalIDinOdoo", "RASxxx", "tz_database_name", "howToDefineTime", "time_format"]
+                for k in keys_to_pop_out:
+                    list_of_all_keys.pop(k)
+
+                for o in list_of_all_keys:
+                    ut.storeOptionInDeviceCustomization(o,answer.get(o, False))
+
+                terminal_ID_in_Odoo     = answer.get('id', False)
+                loggerINFO(f"terminal ID in Odoo: {answer.get('id', False)}")
+                ut.storeOptionInDeviceCustomization("terminalIDinOdoo",answer.get('id', False))
+                ut.storeOptionInDeviceCustomization("RASxxx",getRASxxx()) # TODO getRASxxx should be made in odoo
                 if answer["tz"]:
-                    ut.storeOptionInDeviceCustomization("tz_database_name", answer["tz"])
+                    ut.storeOptionInDeviceCustomization("tz_database_name", answer.get('tz', False))
                     ut.storeOptionInDeviceCustomization("howToDefineTime", "use tz database")
                     cc.setTimeZone()
-                ut.storeOptionInDeviceCustomization("time_format",answer["hour12or24"])
+                ut.storeOptionInDeviceCustomization("time_format",answer.get('hour12or24', False))
                 copyDeviceCustomizationJson()
         else:
             loggerINFO(f"Answer from Odoo did not contain an answer")
@@ -209,9 +209,12 @@ def resetSettings():
         requestURL  = ut.settings["odooUrlTemplate"] + \
             co.ROUTE_INCOMING_IN_ODOO + "/" + ut.settings["routefromDeviceToOdoo"]
         headers     = {'Content-Type': 'application/json'}
-
+        if ut.settings.get("manufacturingData", False):
+            productName = ut.settings.get("manufacturingData").get('productName', False)
+        else:
+            productName = ut.settings.get('productName', False)
         payload     = {'question': co.QUESTION_ASK_FOR_RESET_SETTINGS,
-                    'productName': ut.settings["manufacturingData"].get('productName')}
+                    'productName': productName}
 
         response    = requests.post(url=requestURL, json=payload, headers=headers)
 
@@ -221,8 +224,10 @@ def resetSettings():
         answer = response.json().get("result", None)
         if answer:
             error = answer.get("error", None)
+            ut.storeOptionInDeviceCustomization("isRemoteOdooControlAvailable", True)
             if error:
                 loggerINFO(f"resetSettings not Available - error in answer from Odoo: {error}")
+                return False
             else:
                 loggerINFO(f"resetSettings was successful - {answer}")
                 return True
