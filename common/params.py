dccb30572c7562fd685d1dc018480335af931aaf
lubusax <lu.bu.sax@gmail.com>
2021-05-10 20:54:44 +0200

    auto

--- common/params.py
+++ common/params.py
@@ -2,25 +2,25 @@
 # this file is part of openpilot
 """ROS has a parameter server, we have files.
 
-The parameter store is a persistent key value store, implemented as a directory with a writer lock.
-On Android, we store params under params_dir = /data/params. The writer lock is a file
-"<params_dir>/.lock" taken using flock(), and data is stored in a directory symlinked to by
-"<params_dir>/d".
+  The parameter store is a persistent key value store, implemented as a directory with a writer lock.
+  On Android, we store params under params_dir = /data/params. The writer lock is a file
+  "<params_dir>/.lock" taken using flock(), and data is stored in a directory symlinked to by
+  "<params_dir>/d".
 
-Each key, value pair is stored as a file with named <key> with contents <value>, located in
-  <params_dir>/d/<key>
+  Each key, value pair is stored as a file with named <key> with contents <value>, located in
+    <params_dir>/d/<key>
 
-Readers of a single key can just open("<params_dir>/d/<key>") and read the file contents.
-Readers who want a consistent snapshot of multiple keys should take the lock.
+  Readers of a single key can just open("<params_dir>/d/<key>") and read the file contents.
+  Readers who want a consistent snapshot of multiple keys should take the lock.
 
-Writers should take the lock before modifying anything. Writers should also leave the DB in a
-consistent state after a crash. The implementation below does this by copying all params to a temp
-directory <params_dir>/<tmp>, then atomically symlinking <params_dir>/<d> to <params_dir>/<tmp>
-before deleting the old <params_dir>/<d> directory.
+  Writers should take the lock before modifying anything. Writers should also leave the DB in a
+  consistent state after a crash. The implementation below does this by copying all params to a temp
+  directory <params_dir>/<tmp>, then atomically symlinking <params_dir>/<d> to <params_dir>/<tmp>
+  before deleting the old <params_dir>/<d> directory.
 
-Writers that only modify a single key can simply take the lock, then swap the corresponding value
-file in place without messing with <params_dir>/d.
-"""
+  Writers that only modify a single key can simply take the lock, then swap the corresponding value
+  file in place without messing with <params_dir>/d.
+  """
 
 import time
 import os
@@ -42,13 +42,14 @@ def mkdirs_exists_ok(path):
 
 
 class TxType(Enum):
-  DO_NOT_RESET_ON_MANAGER_START = auto() # do not reset to default values on manager start
-  RESET_ON_MANAGER_START = auto() 
+  #RESET_ON_MANAGER_START = auto() 
   FACTORY_SETTINGS = auto()  # will never change
   TO_DEPRECATE = auto()   # will be deprecated when Odoo Module for Remote Control
                 # is installed and check attendance has worked at least once
   DEFINED_ON_DEVICE_SETUP  = auto() # parameters are defined on device setup
-  UPDATED_FROM_ODOO_ONLY_ON_START = auto()  # parameters updated once 
+  DEFINED_ON_ACK_FROM_ODOO = auto()
+  DEFINED_ON_ACK_FROM_DEVICE = auto()
+  #UPDATED_FROM_ODOO_ONLY_ON_START = auto()  # parameters updated once 
                 # after rebooting the device
                 # and get their values from stored values in the odoo database
                 #  Updates come from Odoo - do not clear on start but
@@ -66,97 +67,126 @@ class UnknownKeyName(Exception):
 json_keys = {
   "installedPythonModules",# make a database and the name of the 
           # files are the name of the modules
-  'incrementalLog', # make a database and the entries are 
-  # 1,2,3 and so on and the file contain the logs
   "messagesDic", # TO_DEPRECATE ##################
   "defaultMessagesDic", # TO_DEPRECATE ##################
   "messages_on_display", #### NEW ---> json file
   "flask", ### to proof if we need it:
 }
 
+factory_settings = [ #[TxType.FACTORY_SETTINGS]
+  "firmwareAtShipment",
+  "productName"       ,
+  "productionDate"    ,
+  "productionLocation",
+  "productionNumber"  ,
+  "qualityInspector"  , 
+  "SSIDreset"         ,
+  "hashed_machine_id" ]
+
+defined_on_ack_from_odoo = [ #[TxType.DEFINED_ON_ACK_FROM_ODOO]
+  "id",
+  "RASxxx",
+  "routefromDeviceToOdoo",
+  "routefromOdooToDevice",
+  "version_things_module_in_Odoo"]
+
+defined_on_ack_from_device = [ #[TxType.DEFINED_ON_ACK_FROM_DEVICE]
+  #"installedPythonModules",
+  "ownIpAddress",
+  "firmwareVersion",
+  "lastFirmwareUpdateTime",
+  "lastTimeTerminalStarted",
+  "updateFailedCount"]
+
+keys_routine_calls = [  #[TxType.UPDATED_FROM_ODOO_ON_ROUTINE_CALLS]
+  "ssh"                             ,
+  "showEmployeeName"                ,
+  "sshPassword"                     ,
+  "language"                        ,
+  "tz"                              ,
+  "time_format"                     ,
+  "timeoutToCheckAttendance"        ,  
+  "periodEvaluateReachability"      ,
+  "periodDisplayClock"              ,
+  "timeToDisplayResultAfterClocking",
+  "location"                        ,
+  "shouldGetFirmwareUpdate"         , # True, False
+  "setRebootAt"                     , # time for next reboot (not periodically - einzelfall nur)
+  'shutdownTerminal'                ,
+  "gitBranch"                       ,
+  "gitCommit"                       ,
+  "gitRemote"                       ,
+  "updateOTAcommand"                ,
+  "doFactoryReset"                  ,
+  "updateAvailable"                 , # to be proofed in Odoo every day @03:00 + random
+  "lastConnectionOdooTerminal" ]
+
 keys = {
   "displayClock":             [TxType.FLAG],
   "acknowledged":             [TxType.FLAG],
-  "firmwareAtShipment":       [TxType.FACTORY_SETTINGS],
-  "productName":              [TxType.FACTORY_SETTINGS],
-  "productionDate":           [TxType.FACTORY_SETTINGS],
-  "productionLocation":       [TxType.FACTORY_SETTINGS],
-  "productionNumber":         [TxType.FACTORY_SETTINGS],
-  "qualityInspector":         [TxType.FACTORY_SETTINGS], 
-  "SSIDreset":                [TxType.FACTORY_SETTINGS],
-  "hashed_machine_id":        [TxType.FACTORY_SETTINGS],  
-  #"fileForMessages":          [TxType.FACTORY_SETTINGS, TxType.TO_DEPRECATE], 
-  #"terminalSetupManagement":  [TxType.FACTORY_SETTINGS, TxType.TO_DEPRECATE], 
-  #"howToDefineTime":          [TxType.FACTORY_SETTINGS, TxType.TO_DEPRECATE],
+  "isRemoteOdooControlAvailable":     [TxType.FLAG],
+  # "firmwareAtShipment":       [TxType.FACTORY_SETTINGS],
+    # "productName":              [TxType.FACTORY_SETTINGS],
+    # "productionDate":           [TxType.FACTORY_SETTINGS],
+    # "productionLocation":       [TxType.FACTORY_SETTINGS],
+    # "productionNumber":         [TxType.FACTORY_SETTINGS],
+    # "qualityInspector":         [TxType.FACTORY_SETTINGS], 
+    # "SSIDreset":                [TxType.FACTORY_SETTINGS],
+    # "hashed_machine_id":        [TxType.FACTORY_SETTINGS],  
   "https":                    [TxType.DEFINED_ON_DEVICE_SETUP],
   "odoo_host":                [TxType.DEFINED_ON_DEVICE_SETUP],
   "odoo_port":                [TxType.DEFINED_ON_DEVICE_SETUP],
   "odooConnectedAtLeastOnce": [TxType.DEFINED_ON_DEVICE_SETUP],
   "odooUrlTemplate":          [TxType.DEFINED_ON_DEVICE_SETUP],
-  #"odooIpPort":               [TxType.DEFINED_ON_DEVICE_SETUP],
   "hasCompletedSetup":        [TxType.DEFINED_ON_DEVICE_SETUP],
-  #"admin_id":                 [TxType.DEFINED_ON_DEVICE_SETUP, TxType.TO_DEPRECATE],
-  #"db":                       [TxType.DEFINED_ON_DEVICE_SETUP, TxType.TO_DEPRECATE],
-  #"user_name":                [TxType.DEFINED_ON_DEVICE_SETUP, TxType.TO_DEPRECATE],
-  #"user_password":            [TxType.DEFINED_ON_DEVICE_SETUP, TxType.TO_DEPRECATE],
-  #"timezone":                 [TxType.DEFINED_ON_DEVICE_SETUP, TxType.TO_DEPRECATE], # to be substituted by "tz_database_name"
-  #TxType.UPDATED_FROM_ODOO_ONLY_ON_START: Updates come from Odoo - do not clear on start but
-  #       can be changed when connecting with odoo (Acknowdledgement)
-  "id":                             [TxType.UPDATED_FROM_ODOO_ONLY_ON_START],
-  "RASxxx":                         [TxType.UPDATED_FROM_ODOO_ONLY_ON_START],
-  "routefromDeviceToOdoo":          [TxType.UPDATED_FROM_ODOO_ONLY_ON_START],
-  "routefromOdooToDevice":          [TxType.UPDATED_FROM_ODOO_ONLY_ON_START],
-  "version_things_module_in_Odoo":  [TxType.UPDATED_FROM_ODOO_ONLY_ON_START],
-  "ownIpAddress":                   [TxType.UPDATED_FROM_ODOO_ONLY_ON_START],
-  #TxType.UPDATED_FROM_ODOO_ON_ROUTINE_CALLS:  Updates come from Odoo - do not clear on start,
-  #       can be changed anytime when connected to Odoo through routine calls
-  "ssh":                              [TxType.UPDATED_FROM_ODOO_ON_ROUTINE_CALLS],
-  "showEmployeeName":                 [TxType.UPDATED_FROM_ODOO_ON_ROUTINE_CALLS],
-  "sshPassword":                      [TxType.UPDATED_FROM_ODOO_ON_ROUTINE_CALLS],
-  "language":                         [TxType.UPDATED_FROM_ODOO_ON_ROUTINE_CALLS],
-  "tz":                               [TxType.UPDATED_FROM_ODOO_ON_ROUTINE_CALLS],
-  "time_format":                      [TxType.UPDATED_FROM_ODOO_ON_ROUTINE_CALLS],
-  "timeoutToCheckAttendance":         [TxType.UPDATED_FROM_ODOO_ON_ROUTINE_CALLS],  
-  "periodEvaluateReachability":       [TxType.UPDATED_FROM_ODOO_ON_ROUTINE_CALLS],
-  "periodDisplayClock":               [TxType.UPDATED_FROM_ODOO_ON_ROUTINE_CALLS],
-  "timeToDisplayResultAfterClocking": [TxType.UPDATED_FROM_ODOO_ON_ROUTINE_CALLS],
-  "location":                         [TxType.UPDATED_FROM_ODOO_ON_ROUTINE_CALLS],
-  "shouldGetFirmwareUpdate":          [TxType.UPDATED_FROM_ODOO_ON_ROUTINE_CALLS], # True, False
-  "setRebootAt":                      [TxType.UPDATED_FROM_ODOO_ON_ROUTINE_CALLS], # time for next reboot (not periodically - einzelfall nur)
-  'shutdownTerminal':                 [TxType.UPDATED_FROM_ODOO_ON_ROUTINE_CALLS],
-  "isRemoteOdooControlAvailable":     [TxType.UPDATED_FROM_DEVICE],
-  "gitBranch":                        [TxType.UPDATED_FROM_ODOO_ON_ROUTINE_CALLS],
-  "gitCommit":                        [TxType.UPDATED_FROM_ODOO_ON_ROUTINE_CALLS],
-  "gitRemote":                        [TxType.UPDATED_FROM_ODOO_ON_ROUTINE_CALLS],
-  "updateOTAcommand":                 [TxType.UPDATED_FROM_ODOO_ON_ROUTINE_CALLS],
-  "doFactoryReset":                   [TxType.UPDATED_FROM_ODOO_ON_ROUTINE_CALLS],
-  "updateAvailable":                  [TxType.UPDATED_FROM_ODOO_ON_ROUTINE_CALLS], # to be proofed in Odoo every day @03:00 + random
-  "timestampLastConnection":          [TxType.UPDATED_FROM_ODOO_ON_ROUTINE_CALLS],
-  #"timeoutToGetOdooUID":              [TxType.UPDATED_FROM_ODOO_ON_ROUTINE_CALLS, TxType.TO_DEPRECATE],  # TO_DEPRECATE ##################
+  # "id":                             [TxType.UPDATED_FROM_ODOO_ONLY_ON_START],
+    # "RASxxx":                         [TxType.UPDATED_FROM_ODOO_ONLY_ON_START],
+    # "routefromDeviceToOdoo":          [TxType.UPDATED_FROM_ODOO_ONLY_ON_START],
+    # "routefromOdooToDevice":          [TxType.UPDATED_FROM_ODOO_ONLY_ON_START],
+    # "version_things_module_in_Odoo":  [TxType.UPDATED_FROM_ODOO_ONLY_ON_START],
 
-  #TxType.UPDATED_FROM_DEVICE: Updates are done through the Firmware
-  #"installedPythonModules": [TxType.UPDATED_FROM_DEVICE],
+  #TxType.UPDATED_FROM_ODOO_ON_ROUTINE_CALLS:  Updates come from Odoo - do not clear on start,
+    #       can be changed anytime when connected to Odoo through routine calls
+    # "ssh":                              [TxType.UPDATED_FROM_ODOO_ON_ROUTINE_CALLS],
+    # "showEmployeeName":                 [TxType.UPDATED_FROM_ODOO_ON_ROUTINE_CALLS],
+    # "sshPassword":                      [TxType.UPDATED_FROM_ODOO_ON_ROUTINE_CALLS],
+    # "language":                         [TxType.UPDATED_FROM_ODOO_ON_ROUTINE_CALLS],
+    # "tz":                               [TxType.UPDATED_FROM_ODOO_ON_ROUTINE_CALLS],
+    # "time_format":                      [TxType.UPDATED_FROM_ODOO_ON_ROUTINE_CALLS],
+    # "timeoutToCheckAttendance":         [TxType.UPDATED_FROM_ODOO_ON_ROUTINE_CALLS],  
+    # "periodEvaluateReachability":       [TxType.UPDATED_FROM_ODOO_ON_ROUTINE_CALLS],
+    # "periodDisplayClock":               [TxType.UPDATED_FROM_ODOO_ON_ROUTINE_CALLS],
+    # "timeToDisplayResultAfterClocking": [TxType.UPDATED_FROM_ODOO_ON_ROUTINE_CALLS],
+    # "location":                         [TxType.UPDATED_FROM_ODOO_ON_ROUTINE_CALLS],
+    # "shouldGetFirmwareUpdate":          [TxType.UPDATED_FROM_ODOO_ON_ROUTINE_CALLS], # True, False
+    # "setRebootAt":                      [TxType.UPDATED_FROM_ODOO_ON_ROUTINE_CALLS], # time for next reboot (not periodically - einzelfall nur)
+    # 'shutdownTerminal':                 [TxType.UPDATED_FROM_ODOO_ON_ROUTINE_CALLS],
+    # "gitBranch":                        [TxType.UPDATED_FROM_ODOO_ON_ROUTINE_CALLS],
+    # "gitCommit":                        [TxType.UPDATED_FROM_ODOO_ON_ROUTINE_CALLS],
+    # "gitRemote":                        [TxType.UPDATED_FROM_ODOO_ON_ROUTINE_CALLS],
+    # "updateOTAcommand":                 [TxType.UPDATED_FROM_ODOO_ON_ROUTINE_CALLS],
+    # "doFactoryReset":                   [TxType.UPDATED_FROM_ODOO_ON_ROUTINE_CALLS],
+    # "updateAvailable":                  [TxType.UPDATED_FROM_ODOO_ON_ROUTINE_CALLS], # to be proofed in Odoo every day @03:00 + random
+    # "timestampLastConnection":          [TxType.UPDATED_FROM_ODOO_ON_ROUTINE_CALLS],
+    #"timeoutToGetOdooUID":              [TxType.UPDATED_FROM_ODOO_ON_ROUTINE_CALLS, TxType.TO_DEPRECATE],  # TO_DEPRECATE ##################
+    #TxType.UPDATED_FROM_DEVICE: Updates are done through the Firmware
+    #"installedPythonModules": [TxType.UPDATED_FROM_DEVICE],
   "incrementalLog":         [TxType.UPDATED_FROM_DEVICE],
-  "firmwareVersion":        [TxType.UPDATED_FROM_DEVICE],
-  "lastFirmwareUpdateTime": [TxType.UPDATED_FROM_DEVICE],
-  "lastTimeTerminalStarted":  [TxType.UPDATED_FROM_DEVICE],
-  "updateFailedCount":      [TxType.UPDATED_FROM_DEVICE],
+  # "ownIpAddress":                   [TxType.UPDATED_FROM_DEVICE],
+    # "firmwareVersion":        [TxType.UPDATED_FROM_DEVICE],
+    # "lastFirmwareUpdateTime": [TxType.UPDATED_FROM_DEVICE],
+    # "lastTimeTerminalStarted":  [TxType.UPDATED_FROM_DEVICE],
+    # "updateFailedCount":      [TxType.UPDATED_FROM_DEVICE],
+  
 }
 
 # ***??? #TO COPY FROM openpilot
-
   #   "GithubSshKeys": [TxType.DO_NOT_RESET_ON_MANAGER_START],
-
   #   "HasAcceptedTerms": [TxType.DO_NOT_RESET_ON_MANAGER_START],
-
   #   "DisablePowerDown": [TxType.DO_NOT_RESET_ON_MANAGER_START],
-
   #   "DisableUpdates": [TxType.DO_NOT_RESET_ON_MANAGER_START],
-
   #   "DoUninstall": [TxType.CLEAR_ON_MANAGER_START],
-
   #   "AccessToken": [TxType.CLEAR_ON_MANAGER_START],
-    
   #   "DongleId": [TxType.DO_NOT_RESET_ON_MANAGER_START],
 
 def fsync_dir(path):
