78484bffdb34c661464a875d4a6f2ba5756ae9d8
lubusax <lu.bu.sax@gmail.com>
2021-06-18 18:39:01 +0200

    auto

--- common/connectivity.py
+++ common/connectivity.py
@@ -1,4 +1,21 @@
+import subprocess
 
+
+def isSuccesRunningSubprocess(command):
+    try:
+        completed = subprocess.run(command.split(),
+            stdout=subprocess.DEVNULL,
+            stderr=subprocess.STDOUT)
+        if completed.returncode == 0:
+            return True
+        else:
+            return False
+    except:
+        return False  
+
+def isPingable(address):
+  command = "ping -c 1 " + address
+  return isSuccesRunningSubprocess(command)
 
 def internetReachable():
   return isPingable("1.1.1.1")
