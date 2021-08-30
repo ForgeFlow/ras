952c51e348f1ca6ef8dc8fc3a6c524e2ebaff7eb
lubusax <lu.bu.sax@gmail.com>
2021-08-30 17:05:59 +0200

    auto

--- odoo/remoteManagement.py
+++ odoo/remoteManagement.py
@@ -25,25 +25,6 @@ def getPayload(settings_to_send):
             loggerERROR(f"Exception while trying to access setting {s}")
     return payload
 
-def getRASxxx():
-    RAS_id_str ="2.1"
-    params = Params(db=PARAMS)
-    try:
-        RAS_id = params.get("terminalIDinOdoo")
-        if RAS_id:
-            RAS_id_str = str(RAS_id)
-            if len(RAS_id_str)==1:
-                RAS_id_str = "00" + RAS_id_str
-            elif len(RAS_id_str)==2:
-                RAS_id_str = "0" + RAS_id_str
-            elif len(RAS_id_str)>3:
-                RAS_id_str = RAS_id_str[-3:]
-        loggerDEBUG(f"RAS_id_str: {RAS_id_str} ")
-    except Exception as e:
-        loggerERROR(f"Exception in getRASxxx: {e}")
-        
-    return RAS_id_str
-
 def acknowledgeTerminalInOdoo():
     terminal_ID_in_Odoo     = False
     params = Params(db=PARAMS)
@@ -78,13 +59,6 @@ def acknowledgeTerminalInOdoo():
                         loggerDEBUG(f"key: {o}; value:{value} ") 
                         if type(value) != bool : value = str(value)
                         params.put(o,value)
-                    terminal_ID_in_Odoo     = answer.get('terminalIDinOdoo', False)
-                    loggerINFO(f"terminal ID in Odoo: {answer.get('terminalIDinOdoo', False)}")
-                    if not terminal_ID_in_Odoo:
-                        terminal_ID_in_Odoo = "2.1"
-                    params.put("terminalIDinOdoo", terminal_ID_in_Odoo)
-                    RASxxx = "RAS-"+getRASxxx()
-                    params.put("RASxxx", RASxxx)
                     if answer["tz"]:
                         loggerDEBUG(f"setting time zone to {answer['tz']}")
                         cc.setTimeZone()
@@ -142,54 +116,6 @@ def isRemoteOdooControlAvailable():
         loggerERROR(f"Remote Odoo Control not Available - Exception: {e}")
     
     return False
-
-# def routineCheck():
-#     try:
-#         requestURL  = params.get("odooUrlTemplate") + \
-#             co.ROUTE_OUTGOING_IN_ODOO + "/" + params.get("routefromOdooToDevice")
-#         headers     = {'Content-Type': 'application/json'}
-#         productName = params.get('productName')
-#         payload     = {'question': co.QUESTION_ASK_FOR_ROUTINE_CHECK,
-#                     'productName': productName,
-#                     'incrementalLog': lo.incrementalLog}
-
-#         response    = requests.post(url=requestURL, json=payload, headers=headers)
-
-#         # print("routineCheck Status code: ", response.status_code)
-#         # print("routineCheck Printing Entire Post Response")
-#         # print(response.json())
-#         answer = response.json().get("result", False)
-#         if answer:
-#             error = answer.get("error", False)
-#             if error:
-#                 loggerINFO(f"Routine Check not Available - error in answer from Odoo: {error}")
-#             else:
-#                 changes = answer.get("changes", False)
-#                 lo.incrementalLog = []
-#                 if changes:
-#                     routine_options_list = params.get_list_of_keys_with_type(TxType.UPDATED_FROM_ODOO_ON_ROUTINE_CALLS)
-#                     routine_options_list.pop("tz_database_name")
-#                     routine_options_list.pop("time_format")
-#                     routine_options_list.pop("isRemoteOdooControlAvailable") 
-#                     routine_options_list.pop("howToDefineTime")                  
-#                     for o in routine_options_list:
-#                         ut.storeOptionInDeviceCustomization(o,answer.get(o, False))
-#                     if answer.get("tz", False) != ut.settings["tz_database_name"]:
-#                         ut.storeOptionInDeviceCustomization("tz_database_name", answer["tz"])
-#                         ut.storeOptionInDeviceCustomization("howToDefineTime", "use tz database")
-#                         cc.setTimeZone()
-#                     ut.storeOptionInDeviceCustomization("time_format",answer.get("hour12or24", False))
-#                     ut.storeOptionInDeviceCustomization("isRemoteOdooControlAvailable", True)                   
-#                 return True
-#         else:
-#             loggerINFO(f"Routine Check not Available - No Answer from Odoo")        
-#     except ConnectionRefusedError as e:
-#         loggerERROR(f"Routine Check not Available - ConnectionRefusedError - Request Exception : {e}")
-#     except Exception as e:
-#         loggerERROR(f"Routine Check not Available - Exception: {e}")
-
-#     ut.storeOptionInDeviceCustomization("isRemoteOdooControlAvailable", False)
-#     return False
 
 def resetSettings():
     try:
